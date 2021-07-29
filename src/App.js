8e5a4065d0f6af7530d28fe30e243c00b38d4f84
Akshay <akshayae8@gmail.com>
2021-07-29 19:09:30 +0530

    basic navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import "./App.css";
 import { useState } from "react";
 import {HelloWorld} from "./pages/HelloWorld";
 import {helloUniverse} from "./pages/helloUniverse";
-import { BrowserRouter,Route } from "react-router-dom";
+import { BrowserRouter,Route, Link } from "react-router-dom";
 
 
 export default function App() {
 
  return (
  <BrowserRouter>
+
+ <Link to="/page1">
+ <h1>Page1</h1>
+ </Link>
+ <Link to="/page2">
+ <h1>Page2</h1>
+ </Link>
+ <Link to="/page3">
+ <h1>Page3</h1>
+ </Link>
+
+ <h1>Something Something</h1>
 <Route exact={true} path="/page1" component ={Page1} />
 <Route exact={true} path="/page2" component ={Page2} />
 <Route exact={true} path="/page3" component ={Page3} />
@@ -23,7 +35,7 @@ export default function App() {
 function Page1(){
   return(
 <div>
-<h1>Page1</h1>
+<h1>Page1 Goa</h1>
 <p>
   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque aspernatur nihil, sed eligendi vel asperiores necessitatibus adipisci corrupti provident non sapiente autem illum doloremque obcaecati esse odio, nisi veritatis minima?
 </p>
@@ -36,7 +48,7 @@ function Page1(){
 function Page2(){
   return(
 <div>
-<h1>Page2</h1>
+<h1>Page2 Dubai</h1>
 <p>
   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque aspernatur nihil, sed eligendi vel asperiores necessitatibus adipisci corrupti provident non sapiente autem illum doloremque obcaecati esse odio, nisi veritatis minima?
 </p>
@@ -48,7 +60,7 @@ function Page2(){
 function Page3(){
   return(
 <div>
-<h1>Page3</h1>
+<h1>Page3 Miami</h1>
 <p>
   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque aspernatur nihil, sed eligendi vel asperiores necessitatibus adipisci corrupti provident non sapiente autem illum doloremque obcaecati esse odio, nisi veritatis minima?
 </p>
