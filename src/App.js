9293ede20ecd03e649db8639f015a8af465c6526
Akshay <akshayae8@gmail.com>
2021-07-29 10:17:39 +0530

    working with map

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title = "Heloo world";
+  let list = ["delhi", "cucutta", "mumbai", "Chennai", "pune", "lonaval"];
   return(
   <div>
-<h1>Thank you</h1>
+<h1>{title}</h1>
+<ul>
+{list.map((item)=>{
+return <li>{item}</li>;
+})}
+</ul>
     </div>
   );
 }
