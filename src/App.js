0daf1a1b80aecb7b287c521edd8b00d8ba4c179d
Akshay <akshayae8@gmail.com>
2021-07-29 19:17:19 +0530

    navigatuon menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import {HelloWorld} from "./pages/HelloWorld";
 import {helloUniverse} from "./pages/helloUniverse";
 import { BrowserRouter,Route, Link } from "react-router-dom";
+import { func } from "prop-types";
 
 
 export default function App() {
@@ -11,25 +12,27 @@ export default function App() {
  return (
  <BrowserRouter>
 
- <Link to="/page1">
- <h1>Page1</h1>
- </Link>
- <Link to="/page2">
- <h1>Page2</h1>
- </Link>
- <Link to="/page3">
- <h1>Page3</h1>
- </Link>
+ <Link to="/page1"> <h1>Page1</h1> </Link>
+ <Link to="/page2"><h1>Page2</h1></Link>
+ <Link to="/page3"><h1>Page3</h1></Link>
+ <Link to="/hw"><h1>Page3</h1></Link>
+ <Link to="/hu"><h1>Page3</h1></Link>
 
  <h1>Something Something</h1>
 <Route exact={true} path="/page1" component ={Page1} />
 <Route exact={true} path="/page2" component ={Page2} />
 <Route exact={true} path="/page3" component ={Page3} />
+<Route exact={true} path="hw" component ={HelloWorld} />
+<Route exact={true} path="/hu" component ={helloUniverse} />
 <Route exact={true} path="/" component ={Page1} />
 
  </BrowserRouter>
  
  );
+}
+
+function NavMenu(){
+  
 }
 
 function Page1(){
