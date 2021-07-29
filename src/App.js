d63626b4331d83a0baa68332c83cc5b39db35adb
Akshay <akshayae8@gmail.com>
2021-07-29 18:24:56 +0530

    app .js is a user defined tag

--- src/App.js
+++ src/App.js
@@ -2,41 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-function App() {
+export default function App() {
 
- let [counter, setCounter] =useState(0);
- let [list, setList] = useState(["Delhi", "mumbai"])
-
- let updateCounter = () =>{
-  //  alert("Something is happening");
-   let newvalue = counter+1;
-   setCounter(newvalue);
- };
-
- let addNewElement = () => {
-  //  alert("Something is ja")
-  let newlist =["I am new Post", ...list];
-  setList(newlist);
- }
-
-
-return (
-  <div>
-    <h1>Statefull counter</h1>
-    <h1>Counter:: {counter} 
-    <input type="button" value="Increment" onClick={updateCounter} />
-    </h1>
-    <hr />
-
-
-    <input type="button" value="Add new Element in list" onClick={addNewElement} />
-
-{list.map((item)=>{
-return <div className="alert alert-success p-2 ">{item}</div>
-})}
-   
-  </div>
-);
+ return (
+ <div>
+      hello world
+ </div>
+ 
+ );
 }
 
 export default App;
