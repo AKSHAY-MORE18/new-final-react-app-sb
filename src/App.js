494338f8da523fc03671d99d6f2008a2e813bc88
Akshay <akshayae8@gmail.com>
2021-07-29 19:33:01 +0530

    user design

--- src/App.js
+++ src/App.js
@@ -9,66 +9,36 @@ import { func } from "prop-types";
 
 export default function App() {
 
- return (
- <BrowserRouter>
-
- <Link to="/page1"> <h1>Page1</h1> </Link>
- <Link to="/page2"><h1>Page2</h1></Link>
- <Link to="/page3"><h1>Page3</h1></Link>
- <Link to="/hw"><h1>Page3</h1></Link>
- <Link to="/hu"><h1>Page3</h1></Link>
-
- <h1>Something Something</h1>
-<Route exact={true} path="/page1" component ={Page1} />
-<Route exact={true} path="/page2" component ={Page2} />
-<Route exact={true} path="/page3" component ={Page3} />
-<Route exact={true} path="hw" component ={HelloWorld} />
-<Route exact={true} path="/hu" component ={helloUniverse} />
-<Route exact={true} path="/" component ={Page1} />
-
- </BrowserRouter>
+ return 
+(
+  <div>
+    <MyComponent/>
+  </div>
+);
  
- );
 }
 
-function NavMenu(){
-  
-}
+function MyComponent(){
+let [list,setList]=useState(["delhi","mumbai"])
 
-function Page1(){
   return(
-<div>
-<h1>Page1 Goa</h1>
-<p>
-  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque aspernatur nihil, sed eligendi vel asperiores necessitatibus adipisci corrupti provident non sapiente autem illum doloremque obcaecati esse odio, nisi veritatis minima?
-</p>
+    <div className="m-2">
+    <h1 >Work with the forms</h1>
+
+    <input type="text" className="form-control form-control-lg my-3"
+      placeholder="post your thoughts"/>
+
+    <input type="button" className="btn btn-primary w-100mt-1" value="post your thought" />
+
+    {list.map((item) => {
+        return <div className="alert alert-primary mt-2">Delhi</div>
+    })}
+
+   <div className="h1 bg-primary my-1 p-2 text-light">AThought list</div>
+
+
   </div>
-  )
-}
-
-
-
-function Page2(){
-  return(
-<div>
-<h1>Page2 Dubai</h1>
-<p>
-  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque aspernatur nihil, sed eligendi vel asperiores necessitatibus adipisci corrupti provident non sapiente autem illum doloremque obcaecati esse odio, nisi veritatis minima?
-</p>
-  </div>
-  )
-}
-
-
-function Page3(){
-  return(
-<div>
-<h1>Page3 Miami</h1>
-<p>
-  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque aspernatur nihil, sed eligendi vel asperiores necessitatibus adipisci corrupti provident non sapiente autem illum doloremque obcaecati esse odio, nisi veritatis minima?
-</p>
-  </div>
-  )
+  );
 }
 
 
